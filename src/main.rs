287e6e02fe43a2a50a977e02dd3506ff87f33227
LOU Xun <aquarhead@ela.build>
2020-02-05 23:29:35 +0000

    Vertex shader compiled

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use glfw::Context;
 use glfw::{Action, Key, OpenGlProfileHint, WindowHint, WindowMode};
+use std::ffi::CString;
 use std::mem;
+use std::ptr::{null, null_mut};
 
 fn main() {
   let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
@@ -20,6 +22,7 @@ fn main() {
     window.set_framebuffer_size_polling(true);
     window.set_key_polling(true);
 
+    // Vertex Data
     let vertices: Vec<f32> = vec![-0.5, 0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
     let mut vbo = 0;
     unsafe {
@@ -31,6 +34,36 @@ fn main() {
         vertices.as_ptr() as *const std::ffi::c_void,
         gl::STATIC_DRAW,
       );
+    }
+
+    // Vertex Shader
+    let vs_code = CString::new(
+      "
+#version 460 core
+layout (location=0) in vec3 aPos;
+
+void main() {
+  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}",
+    )
+    .unwrap();
+
+    unsafe {
+      let vs_id = gl::CreateShader(gl::VERTEX_SHADER);
+      gl::ShaderSource(vs_id, 1, &vs_code.as_c_str().as_ptr(), null());
+      gl::CompileShader(vs_id);
+
+      let mut success = 0;
+      let mut log = [0; 512];
+
+      gl::GetShaderiv(vs_id, gl::COMPILE_STATUS, &mut success);
+      if success as u8 != gl::TRUE {
+        gl::GetShaderInfoLog(vs_id, 512, null_mut(), log.as_mut_ptr());
+        panic!(
+          "Vertex Shader failed to compile:\n{}",
+          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
+        );
+      }
     }
 
     while !window.should_close() {
