d495c199e74dcb7433e31e4dfb0aa3a772fdbc66
LOU Xun <aquarhead@ela.build>
2020-02-05 22:29:07 +0000

    Use my formatter style

--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,45 @@ use glfw::Context;
 use glfw::{Action, Key, OpenGlProfileHint, WindowHint, WindowMode};
 
 fn main() {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    glfw.window_hint(WindowHint::ContextVersion(4, 6));
-    glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
+  let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+  glfw.window_hint(WindowHint::ContextVersion(4, 6));
+  glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
 
-    if let Some((mut window, events)) =
-        glfw.create_window(800, 600, "Rust-LearnOpenGL", WindowMode::Windowed)
-    {
-        window.make_current();
+  if let Some((mut window, events)) = glfw.create_window(800, 600, "Rust-LearnOpenGL", WindowMode::Windowed) {
+    window.make_current();
 
-        gl_loader::init_gl();
-        gl::load_with(|symbol| gl_loader::get_proc_address(symbol) as *const _);
+    gl_loader::init_gl();
+    gl::load_with(|symbol| gl_loader::get_proc_address(symbol) as *const _);
 
-        unsafe {
-            gl::Viewport(0, 0, 800, 600);
-        }
-
-        window.set_framebuffer_size_polling(true);
-        window.set_key_polling(true);
-
-        while !window.should_close() {
-            glfw.poll_events();
-            for (_, event) in glfw::flush_messages(&events) {
-                println!("{:?}", event);
-
-                match event {
-                    glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
-                        gl::Viewport(0, 0, width, height);
-                    },
-                    glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                        window.set_should_close(true)
-                    }
-                    _ => {}
-                }
-            }
-
-            unsafe {
-                gl::ClearColor(0.2, 0.3, 0.3, 1.0);
-                gl::Clear(gl::COLOR_BUFFER_BIT);
-            }
-
-            window.swap_buffers();
-        }
-    } else {
-        panic!("failed to create GLFW window");
+    unsafe {
+      gl::Viewport(0, 0, 800, 600);
     }
+
+    window.set_framebuffer_size_polling(true);
+    window.set_key_polling(true);
+
+    while !window.should_close() {
+      glfw.poll_events();
+      for (_, event) in glfw::flush_messages(&events) {
+        println!("{:?}", event);
+
+        match event {
+          glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+            gl::Viewport(0, 0, width, height);
+          },
+          glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
+          _ => {}
+        }
+      }
+
+      unsafe {
+        gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+      }
+
+      window.swap_buffers();
+    }
+  } else {
+    panic!("failed to create GLFW window");
+  }
 }
