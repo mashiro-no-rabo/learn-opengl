e9fa53aa3acfc2f15801fc7bb84e3d6a714d696c
LOU Xun <aquarhead@ela.build>
2020-02-08 01:27:02 +0000

    Add ShaderProgram struct
    - Also exercises 1 & 2 in the Shader chapter

--- src/bin/shader.rs
+++ src/bin/shader.rs
@@ -1,8 +1,9 @@
 use glfw::Context;
 use glfw::{Action, Key, OpenGlProfileHint, WindowHint, WindowMode};
-use std::ffi::{c_void, CString};
+use std::ffi::c_void;
 use std::mem;
-use std::ptr::{null, null_mut};
+
+use learn_opengl::{ShaderProgram, UniformValue};
 
 fn main() {
   let mut wireframe_mode = false;
@@ -36,99 +37,34 @@ fn main() {
     println!("Max Vertex Attributes: {}", max_vertex_attributes);
 
     // Vertex Shader
-    let vs_code = CString::new(
-      "
+    let vs_code = "
 #version 460 core
 layout (location=0) in vec3 aPos;
 layout (location=1) in vec3 aColor;
 
+uniform float xOffset;
+
 out vec3 vtxColor;
 
 void main() {
-  gl_Position = vec4(aPos, 1.0);
+  gl_Position = vec4(aPos.x + xOffset, -aPos.y, aPos.z, 1.0);
   vtxColor = aColor;
-}",
-    )
-    .unwrap();
-
-    let vs = unsafe {
-      let vs_id = gl::CreateShader(gl::VERTEX_SHADER);
-      gl::ShaderSource(vs_id, 1, &vs_code.as_c_str().as_ptr(), null());
-      gl::CompileShader(vs_id);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetShaderiv(vs_id, gl::COMPILE_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetShaderInfoLog(vs_id, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Vertex Shader failed to compile:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      vs_id
-    };
+}";
 
     // Fragment Shader
-    let fs_code = CString::new(
-      "
+    let fs_code = "
 #version 460 core
-out vec4 FragColor;
 in vec3 vtxColor;
+uniform float uAlpha;
+
+out vec4 FragColor;
 
 void main() {
-  FragColor = vec4(vtxColor, 1.0);
-}
-",
-    )
-    .unwrap();
-
-    let fs = unsafe {
-      let fs_id = gl::CreateShader(gl::FRAGMENT_SHADER);
-      gl::ShaderSource(fs_id, 1, &fs_code.as_c_str().as_ptr(), null());
-      gl::CompileShader(fs_id);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetShaderiv(fs_id, gl::COMPILE_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetShaderInfoLog(fs_id, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Fragment Shader failed to compile:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      fs_id
-    };
+  FragColor = vec4(vtxColor, uAlpha);
+}";
 
     // Shader Program
-    let sp = unsafe {
-      let sp = gl::CreateProgram();
-      gl::AttachShader(sp, vs);
-      gl::AttachShader(sp, fs);
-      gl::LinkProgram(sp);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetProgramiv(sp, gl::LINK_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetProgramInfoLog(sp, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Shader Program failed to link:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      gl::DeleteShader(vs);
-      gl::DeleteShader(fs);
-
-      sp
-    };
+    let sp = unsafe { ShaderProgram::from_str(vs_code, fs_code) };
 
     // Vertex Data
     let va_triangle = unsafe {
@@ -200,7 +136,11 @@ void main() {
         gl::ClearColor(0.2, 0.3, 0.3, 1.0);
         gl::Clear(gl::COLOR_BUFFER_BIT);
 
-        gl::UseProgram(sp);
+        sp.use_program();
+        sp.set_uniform_value("xOffset", 0.3f32);
+
+        // TODO: this doesn't work
+        sp.set_uniform_value("uAlpha", 0.2f32);
 
         gl::BindVertexArray(va_triangle);
         gl::DrawArrays(gl::TRIANGLES, 0, 3);
---
+++ src/lib.rs
@@ -0,0 +1,119 @@
+use std::ffi::CString;
+use std::fs;
+use std::ptr::{null, null_mut};
+
+pub struct ShaderProgram {
+  id: u32,
+}
+
+impl ShaderProgram {
+  pub unsafe fn from_str(vss: &str, fss: &str) -> Self {
+    // Vertex Shader
+    let vsc = CString::new(vss).unwrap();
+    let vsp = &vsc.as_c_str().as_ptr();
+
+    let vs = {
+      let vs_id = gl::CreateShader(gl::VERTEX_SHADER);
+      gl::ShaderSource(vs_id, 1, vsp, null());
+      gl::CompileShader(vs_id);
+
+      let mut success = 0;
+      let mut log = [0; 512];
+
+      gl::GetShaderiv(vs_id, gl::COMPILE_STATUS, &mut success);
+      if success as u8 != gl::TRUE {
+        gl::GetShaderInfoLog(vs_id, 512, null_mut(), log.as_mut_ptr());
+        panic!(
+          "Vertex Shader failed to compile:\n{}",
+          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
+        );
+      }
+
+      vs_id
+    };
+
+    // Fragment Shader
+    let fsc = CString::new(fss).unwrap();
+    let fsp = &fsc.as_c_str().as_ptr();
+
+    let fs = {
+      let fs_id = gl::CreateShader(gl::FRAGMENT_SHADER);
+      gl::ShaderSource(fs_id, 1, fsp, null());
+      gl::CompileShader(fs_id);
+
+      let mut success = 0;
+      let mut log = [0; 512];
+
+      gl::GetShaderiv(fs_id, gl::COMPILE_STATUS, &mut success);
+      if success as u8 != gl::TRUE {
+        gl::GetShaderInfoLog(fs_id, 512, null_mut(), log.as_mut_ptr());
+        panic!(
+          "Fragment Shader failed to compile:\n{}",
+          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
+        );
+      }
+
+      fs_id
+    };
+
+    // Shader Program
+    let sp = {
+      let sp = gl::CreateProgram();
+      gl::AttachShader(sp, vs);
+      gl::AttachShader(sp, fs);
+      gl::LinkProgram(sp);
+
+      let mut success = 0;
+      let mut log = [0; 512];
+
+      gl::GetProgramiv(sp, gl::LINK_STATUS, &mut success);
+      if success as u8 != gl::TRUE {
+        gl::GetProgramInfoLog(sp, 512, null_mut(), log.as_mut_ptr());
+        panic!(
+          "Shader Program failed to link:\n{}",
+          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
+        );
+      }
+
+      gl::DeleteShader(vs);
+      gl::DeleteShader(fs);
+
+      sp
+    };
+    Self { id: sp }
+  }
+
+  pub unsafe fn from_file(vsf: &str, fsf: &str) -> Self {
+    let vs_code = fs::read_to_string(vsf).unwrap();
+    let fs_code = fs::read_to_string(fsf).unwrap();
+
+    Self::from_str(&vs_code, &fs_code)
+  }
+
+  pub unsafe fn use_program(&self) {
+    gl::UseProgram(self.id);
+  }
+
+  pub unsafe fn set_uniform_value(&self, name: &str, value: impl UniformValue) {
+    let name = CString::new(name).unwrap();
+    let np = name.as_c_str().as_ptr();
+    let loc = gl::GetUniformLocation(self.id, np);
+    value.gl_uniform(loc);
+  }
+}
+
+pub trait UniformValue: Copy {
+  unsafe fn gl_uniform(self, location: i32);
+}
+
+impl UniformValue for bool {
+  unsafe fn gl_uniform(self, location: i32) {
+    gl::Uniform1i(location, self as i32);
+  }
+}
+
+impl UniformValue for f32 {
+  unsafe fn gl_uniform(self, location: i32) {
+    gl::Uniform1f(location, self);
+  }
+}
