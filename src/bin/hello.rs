2d14f6a6ff5855682e60db466997b833eed68c76
LOU Xun <aquarhead@ela.build>
2020-02-17 20:12:34 +0000

    Update old code to use ShaderProgram struct

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,8 +1,9 @@
 use glfw::Context;
 use glfw::{Action, Key, OpenGlProfileHint, WindowHint, WindowMode};
-use std::ffi::{c_void, CString};
+use std::ffi::c_void;
 use std::mem;
-use std::ptr::{null, null_mut};
+
+use learn_opengl::ShaderProgram;
 
 fn main() {
   let mut wireframe_mode = false;
@@ -30,94 +31,25 @@ fn main() {
     window.set_key_polling(true);
 
     // Vertex Shader
-    let vs_code = CString::new(
-      "
+    let vs_code = "
 #version 460 core
 layout (location=0) in vec3 aPos;
 
 void main() {
   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-}",
-    )
-    .unwrap();
-
-    let vs = unsafe {
-      let vs_id = gl::CreateShader(gl::VERTEX_SHADER);
-      gl::ShaderSource(vs_id, 1, &vs_code.as_c_str().as_ptr(), null());
-      gl::CompileShader(vs_id);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetShaderiv(vs_id, gl::COMPILE_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetShaderInfoLog(vs_id, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Vertex Shader failed to compile:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      vs_id
-    };
+}";
 
     // Fragment Shader
-    let fs_code = CString::new(
-      "
+    let fs_code = "
 #version 460 core
 out vec4 FragColor;
 
 void main() {
   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-}
-",
-    )
-    .unwrap();
-
-    let fs = unsafe {
-      let fs_id = gl::CreateShader(gl::FRAGMENT_SHADER);
-      gl::ShaderSource(fs_id, 1, &fs_code.as_c_str().as_ptr(), null());
-      gl::CompileShader(fs_id);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetShaderiv(fs_id, gl::COMPILE_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetShaderInfoLog(fs_id, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Fragment Shader failed to compile:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      fs_id
-    };
+}";
 
     // Shader Program
-    let sp = unsafe {
-      let sp = gl::CreateProgram();
-      gl::AttachShader(sp, vs);
-      gl::AttachShader(sp, fs);
-      gl::LinkProgram(sp);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetProgramiv(sp, gl::LINK_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetProgramInfoLog(sp, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Shader Program failed to link:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      gl::DeleteShader(vs);
-      gl::DeleteShader(fs);
-
-      sp
-    };
+    let sp = unsafe { ShaderProgram::from_str(vs_code, fs_code) };
 
     // Vertex Data
     let va_triangle = unsafe {
@@ -188,7 +120,8 @@ void main() {
         gl::ClearColor(0.2, 0.3, 0.3, 1.0);
         gl::Clear(gl::COLOR_BUFFER_BIT);
 
-        gl::UseProgram(sp);
+        sp.use_program();
+
         gl::BindVertexArray(va_triangle);
         gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, 0 as *const c_void);
       }
--- src/bin/hello_triangle_ex.rs
+++ src/bin/hello_triangle_ex.rs
@@ -1,8 +1,9 @@
 use glfw::Context;
 use glfw::{Action, Key, OpenGlProfileHint, WindowHint, WindowMode};
-use std::ffi::{c_void, CString};
+use std::ffi::c_void;
 use std::mem;
-use std::ptr::{null, null_mut};
+
+use learn_opengl::ShaderProgram;
 
 fn main() {
   let mut wireframe_mode = false;
@@ -30,150 +31,36 @@ fn main() {
     window.set_key_polling(true);
 
     // Vertex Shader
-    let vs_code = CString::new(
-      "
+    let vs_code = "
 #version 460 core
 layout (location=0) in vec3 aPos;
 
 void main() {
   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-}",
-    )
-    .unwrap();
-
-    let vs = unsafe {
-      let vs_id = gl::CreateShader(gl::VERTEX_SHADER);
-      gl::ShaderSource(vs_id, 1, &vs_code.as_c_str().as_ptr(), null());
-      gl::CompileShader(vs_id);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetShaderiv(vs_id, gl::COMPILE_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetShaderInfoLog(vs_id, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Vertex Shader failed to compile:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      vs_id
-    };
+}";
 
     // Fragment Shader
-    let fs_code1 = CString::new(
-      "
+    let fs_code1 = "
 #version 460 core
 out vec4 FragColor;
 
 void main() {
   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
-",
-    )
-    .unwrap();
+";
 
-    let fs1 = unsafe {
-      let fs_id = gl::CreateShader(gl::FRAGMENT_SHADER);
-      gl::ShaderSource(fs_id, 1, &fs_code1.as_c_str().as_ptr(), null());
-      gl::CompileShader(fs_id);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetShaderiv(fs_id, gl::COMPILE_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetShaderInfoLog(fs_id, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Fragment Shader 1 failed to compile:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      fs_id
-    };
-
-    let fs_code2 = CString::new(
-      "
+    let fs_code2 = "
 #version 460 core
 out vec4 FragColor;
 
 void main() {
   FragColor = vec4(0.99607f, 0.87450f, 88235f, 1.0f);
 }
-",
-    )
-    .unwrap();
+";
 
-    let fs2 = unsafe {
-      let fs_id = gl::CreateShader(gl::FRAGMENT_SHADER);
-      gl::ShaderSource(fs_id, 1, &fs_code2.as_c_str().as_ptr(), null());
-      gl::CompileShader(fs_id);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetShaderiv(fs_id, gl::COMPILE_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetShaderInfoLog(fs_id, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Fragment Shader 2 failed to compile:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      fs_id
-    };
-
-    // Shader Program
-    let sp1 = unsafe {
-      let sp = gl::CreateProgram();
-      gl::AttachShader(sp, vs);
-      gl::AttachShader(sp, fs1);
-      gl::LinkProgram(sp);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetProgramiv(sp, gl::LINK_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetProgramInfoLog(sp, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Shader Program failed to link:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      sp
-    };
-
-    let sp2 = unsafe {
-      let sp = gl::CreateProgram();
-      gl::AttachShader(sp, vs);
-      gl::AttachShader(sp, fs2);
-      gl::LinkProgram(sp);
-
-      let mut success = 0;
-      let mut log = [0; 512];
-
-      gl::GetProgramiv(sp, gl::LINK_STATUS, &mut success);
-      if success as u8 != gl::TRUE {
-        gl::GetProgramInfoLog(sp, 512, null_mut(), log.as_mut_ptr());
-        panic!(
-          "Shader Program failed to link:\n{}",
-          String::from_utf8(log.into_iter().map(|x| *x as u8).collect()).unwrap()
-        );
-      }
-
-      sp
-    };
-
-    unsafe {
-      gl::DeleteShader(vs);
-      gl::DeleteShader(fs1);
-      gl::DeleteShader(fs2);
-    }
+    // Shader Programs
+    let sp1 = unsafe { ShaderProgram::from_str(vs_code, fs_code1) };
+    let sp2 = unsafe { ShaderProgram::from_str(vs_code, fs_code2) };
 
     // Triangle 1
     let t1 = unsafe {
@@ -272,12 +159,14 @@ void main() {
         gl::ClearColor(0.2, 0.3, 0.3, 1.0);
         gl::Clear(gl::COLOR_BUFFER_BIT);
 
-        gl::UseProgram(sp1);
+        // Draw Triangle 1
+        sp1.use_program();
 
         gl::BindVertexArray(t1);
         gl::DrawArrays(gl::TRIANGLES, 0, 3);
 
-        gl::UseProgram(sp2);
+        // Draw Triangle 2
+        sp2.use_program();
 
         gl::BindVertexArray(t2);
         gl::DrawArrays(gl::TRIANGLES, 0, 3);
