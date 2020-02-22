dc266d065b9273bd25ad2829734f175214dd1fd9
LOU Xun <aquarhead@ela.build>
2020-02-22 21:19:17 +0000

    Add mouse controlling pitch and yaw

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use glfw::Context;
-use glfw::{Action, Key, OpenGlProfileHint, WindowHint, WindowMode};
+use glfw::{OpenGlProfileHint, WindowHint, WindowMode};
 use nalgebra_glm as glm;
 use std::ffi::c_void;
 use std::mem;
 
 use strugl::{deg_to_rad, Matrix4, ShaderProgram};
+
+const INIT_WIDTH: u32 = 800;
+const INIT_HEIGHT: u32 = 600;
 
 fn main() {
   let mut wireframe_mode = false;
@@ -18,16 +21,20 @@ fn main() {
   glfw.window_hint(WindowHint::ContextVersion(4, 6));
   glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
 
-  if let Some((mut window, events)) = glfw.create_window(800, 600, "Rust-LearnOpenGL", WindowMode::Windowed) {
+  if let Some((mut window, events)) =
+    glfw.create_window(INIT_WIDTH, INIT_HEIGHT, "Rust-LearnOpenGL", WindowMode::Windowed)
+  {
     window.make_current();
     window.set_framebuffer_size_polling(true);
     window.set_key_polling(true);
+    window.set_cursor_pos_polling(true);
+    window.set_cursor_mode(glfw::CursorMode::Disabled);
 
     gl_loader::init_gl();
     gl::load_with(|symbol| gl_loader::get_proc_address(symbol) as *const _);
 
     unsafe {
-      gl::Viewport(0, 0, 800, 600);
+      gl::Viewport(0, 0, INIT_WIDTH as i32, INIT_HEIGHT as i32);
     }
 
     unsafe {
@@ -240,13 +247,19 @@ void main()
 
     // Camera
     let mut camera_pos = glm::vec3(0.0, 0.0, 3.0);
-    let camera_front = glm::vec3(0.0, 0.0, -1.0);
+    let mut camera_front = glm::vec3(0.0, 0.0, -1.0);
     let camera_up = glm::vec3(0.0, 1.0, 0.0);
 
     // Interaction
     let mut mix_value = 0.2f32;
+
     let camera_speed = 2.5f32;
     let mut last_time = 0.0;
+
+    let mut last_mouse = None;
+    let mouse_sensitivity = 0.05;
+    let mut pitch_deg = 0.0f32;
+    let mut yaw_deg = -90.0f32;
 
     // Loop
     while !window.should_close() {
@@ -258,20 +271,37 @@ void main()
 
       glfw.poll_events();
       for (_, event) in glfw::flush_messages(&events) {
+        use glfw::{Action::*, Key::*, WindowEvent::*};
+
         match event {
-          glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+          FramebufferSize(width, height) => unsafe {
             gl::Viewport(0, 0, width, height);
           },
-          glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
-          glfw::WindowEvent::Key(Key::Up, _, Action::Press, _) => mix_value = (mix_value + 0.1).min(1.0),
-          glfw::WindowEvent::Key(Key::Down, _, Action::Press, _) => mix_value = (mix_value - 0.1).max(0.0),
-          glfw::WindowEvent::Key(Key::W, _, Action::Repeat, _) => camera_pos += camera_movement * camera_front,
-          glfw::WindowEvent::Key(Key::S, _, Action::Repeat, _) => camera_pos -= camera_movement * camera_front,
-          glfw::WindowEvent::Key(Key::A, _, Action::Repeat, _) => {
+          Key(Escape, _, Press, _) => window.set_should_close(true),
+          Key(Up, _, Press, _) => mix_value = (mix_value + 0.1).min(1.0),
+          Key(Down, _, Press, _) => mix_value = (mix_value - 0.1).max(0.0),
+          Key(W, _, Repeat, _) => camera_pos += camera_movement * camera_front,
+          Key(S, _, Repeat, _) => camera_pos -= camera_movement * camera_front,
+          Key(A, _, Repeat, _) => {
             camera_pos -= camera_movement * glm::normalize(&glm::cross(&camera_front, &camera_up))
           }
-          glfw::WindowEvent::Key(Key::D, _, Action::Repeat, _) => {
+          Key(D, _, Repeat, _) => {
             camera_pos += camera_movement * glm::normalize(&glm::cross(&camera_front, &camera_up))
+          }
+          CursorPos(x, y) => {
+            if let Some((last_x, last_y)) = last_mouse {
+              let offset_x = (x - last_x) as f32 * mouse_sensitivity;
+              let offset_y = (y - last_y) as f32 * mouse_sensitivity;
+              pitch_deg = (pitch_deg - offset_y).min(60.0).max(-60.0);
+              yaw_deg = yaw_deg + offset_x;
+
+              let dir_x = (deg_to_rad(yaw_deg) * deg_to_rad(pitch_deg).cos()).cos();
+              let dir_y = deg_to_rad(pitch_deg).sin();
+              let dir_z = (deg_to_rad(yaw_deg) * deg_to_rad(pitch_deg).cos()).sin();
+
+              camera_front = glm::normalize(&glm::vec3(dir_x, dir_y, dir_z));
+            }
+            last_mouse = Some((x, y));
           }
           _ => {}
         }
